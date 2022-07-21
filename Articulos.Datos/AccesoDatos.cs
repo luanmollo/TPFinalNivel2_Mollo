1ff653a55ff8f89b4ca7eeee09bd29a6485563bb
Elias Mollo <emollo@oficinacys.com.ar>
2022-07-21 14:30:34 -0300

    agrego clase de acceso a datos

---
+++ Articulos.Datos/AccesoDatos.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Articulos.Datos
+{
+    public class AccesoDatos
+    {
+        private SqlConnection conexion;
+        private SqlCommand comando;
+        private SqlDataReader lector;
+        public SqlDataReader Lector { get; }
+
+        public AccesoDatos()
+        {
+            conexion = new SqlConnection(ConfigurationManager.AppSettings["dbInfo"]);
+            comando = new SqlCommand();
+        }
+
+        public void ConfigurarConsulta(string consulta)
+        {
+            comando.CommandType = System.Data.CommandType.Text;
+            comando.CommandText = consulta;
+        }
+
+        public void EjecutarLectura()
+        {
+            comando.Connection = conexion;
+
+            try
+            {
+                conexion.Open();
+                lector = comando.ExecuteReader();
+
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+        }
+
+        public void EjecutarAccion()
+        {
+            comando.Connection = conexion;
+
+            try
+            {
+                conexion.Open();
+                comando.ExecuteNonQuery();
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+        }
+
+        public void ConfigurarParametros(string nombre, object valor)
+        {
+            comando.Parameters.AddWithValue(nombre, valor);
+        }
+
+        public void CerrarConexion()
+        {
+            if(lector != null)
+            {
+                lector.Close();
+            }
+
+            conexion.Close();
+        }
+
+    }
+}
